a10b6130d4b071de1b301806a384ceae5190adb9
ProfessorChaswick <112112334+ProfessorChaswick@users.noreply.github.com>
2022-08-28 15:01:27 -0400

    Add project files.

---
+++ RegisterAndLoginProj/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RegisterAndLoginProj.Models;
+
+namespace RegisterAndLoginProj.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if(userModel.UserName == "BillGates" && userModel.Password == "bigbucks")
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else 
+            { 
+                return View("LoginFailure", userModel); 
+            }
+            
+        }
+    }
+}
