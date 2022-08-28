08e8787ac0b924b7d2890a330d04f9421d419604
ProfessorChaswick <112112334+ProfessorChaswick@users.noreply.github.com>
2022-08-28 17:34:49 -0400

    Added database stuff

--- RegisterAndLoginProj/Models/UserModel.cs
+++ RegisterAndLoginProj/Models/UserModel.cs
@@ -3,8 +3,8 @@
     public class UserModel
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
 
     }
 }
---
+++ RegisterAndLoginProj/Services/UserDAO.cs
@@ -0,0 +1,45 @@
+﻿//using AspNetCore;
+using RegisterAndLoginProj.Models;
+using System.Data.SqlClient;
+
+namespace RegisterAndLoginProj.Services
+{
+    public class UserDAO
+    {
+        bool success = false;
+        string connectionString = @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    
+        public bool FindUserByNameAndPassword(UserModel user)
+        {
+            /* return true if found in the db
+            Find documentation for all this stuff in microsoft docs undet
+            SqlCommand.Parameters Property
+            */
+            string sqlStatement = "Select * from dbo.Users where username = @username AND password = @password";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(sqlStatement, connection);
+
+                cmd.Parameters.Add("@username", System.Data.SqlDbType.VarChar, 40).Value = user.UserName;
+                cmd.Parameters.Add("@password", System.Data.SqlDbType.VarChar, 40).Value = user.Password;
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if(reader.HasRows)
+                    {
+                        success = true;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            return success;
+        }
+    }
+
+}
