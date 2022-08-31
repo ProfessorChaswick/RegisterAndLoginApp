b5db941db5e996837fba7b7403e84194c861ca15
Charles Hildebrant <ceh2624@gmail.com>
2022-08-31 10:14:24 -0400

    To fix complaints from the db I set TrustServerCertificate to true in the connection string.  Now the db connection is happy.

--- RegisterAndLoginProj/Services/UserDAO.cs
+++ RegisterAndLoginProj/Services/UserDAO.cs
@@ -7,7 +7,7 @@ namespace RegisterAndLoginProj.Services
     public class UserDAO
     {
         bool success = false;
-        string connectionString = @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CodingSchool;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
     
         public bool FindUserByNameAndPassword(UserModel user)
         {
@@ -15,7 +15,7 @@ namespace RegisterAndLoginProj.Services
             Find documentation for all this stuff in microsoft docs undet
             SqlCommand.Parameters Property
             */
-            string sqlStatement = @"Select * from dbo.Users where UserName = @username AND Password = @password";
+            string sqlStatement = @"Select * from dbo.test where UserName = @username AND Password = @password";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
